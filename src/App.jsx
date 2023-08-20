ffa4bb82c9aadd0623dec5a2465ffe8667947419
Ali Abualhawa <aliabualhawa7@gmail.com>
2023-08-20 13:34:19 -0500

    Added App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 
-import './App.css'
+// import './App.css'
 
-function App() {
+// function App() {
  
+//   return (
+//     <div>
+
+//     </div>
+//   )
+// }
+
+// export default App
+
+// src/App.js
+import React from 'react';
+import TeamList from './TeamList';
+import './App.css'
+function App() {
   return (
-    <div>
-      
+    <div className="App">
+      <h1 className='center'>NBA Teams</h1>
+      <TeamList />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
