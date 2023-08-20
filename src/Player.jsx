8a27da4fef10e5d17f79f137868c0e2803884dcd
Ali Abualhawa <aliabualhawa7@gmail.com>
2023-08-19 21:47:48 -0500

    Added Data

---
+++ src/Player.jsx
@@ -0,0 +1,87 @@
+// src/data.js
+export const nbaTeams = [
+    {
+      teamName: 'Los Angeles Lakers',
+      city: 'Los Angeles',
+      players: [
+        { name: 'LeBron James', position: 'Forward' },
+        { name: 'Anthony Davis', position: 'Forward/Center' },
+      ],
+    },
+    {
+      teamName: 'Phonix Suns',
+      city: 'Phonix',
+      players: [
+        { name: 'Kevin Durant', position: 'Forward' },
+        { name: 'Devin Booker', position: 'Guard' },
+      ],
+    },
+    {
+      teamName: 'Chicago Bulls',
+      city: 'Chicago',
+      players: [
+        { name: 'Zach LaVine', position: 'Guard' },
+        { name: 'Nikola Vucevic', position: 'Center' },
+      ],
+    },
+    {
+      teamName: 'Golden State Warriors',
+      city: 'Golden State',
+      players: [
+        { name: 'Stephen Curry', position: 'Guard' },
+        { name: 'Klay Thompson', position: 'Guard' },
+      ],
+    },
+  
+    {
+      teamName: 'Houston Rockets',
+      city: 'Houston',
+      players: [
+        { name: 'John Wall', position: 'Guard' },
+        { name: 'Christian Wood', position: 'Forward/Center' },
+      ],
+    },
+    {
+      teamName: 'Miami Heat',
+      city: 'Miami',
+      players: [
+        { name: 'Jimmy Butler', position: 'Guard/Forward' },
+        { name: 'Bam Adebayo', position: 'Center' },
+      ],
+    },
+    {
+      teamName: 'Philadelphia 76ers',
+      city: 'Philadelphia',
+      players: [
+        { name: 'Joel Embiid', position: 'Center' },
+        { name: 'Ben Simmons', position: 'Guard/Forward' },
+      ],
+    },
+
+    {
+        teamName: 'Boston Celtics',
+        city: 'Boston',
+        players: [
+          { name: 'Jasyon Tatum', position: 'Forward' },
+          { name: 'Jaylen Brown', position: 'Guard/Forward' },
+        ],
+      },
+
+    
+      {
+        teamName: 'NewYork Knicks',
+        city: 'NewYork',
+        players: [
+          { name: 'Julis Randal', position: 'Powerforward' },
+          { name: 'Jalen Brunson', position: 'Guard' },
+        ],
+      },
+      {
+        teamName: 'Denver Nuggets',
+        city: 'Denver',
+        players: [
+          { name: 'Nikola Jovic', position: 'Center' },
+          { name: 'Jamal Murrary', position: 'Guard' },
+        ],
+      },
+      
